d34e6ef34ccdd6e048a2aa25fff3da2cb21f0591
Winter Hardcastle <winter@hardcastle.io>
2023-03-01 18:07:44 -0500

    Added first Three canvas / custom object

---
+++ src/components/Background.tsx
@@ -0,0 +1,16 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import {Speaker} from './Speaker'
+
+export const Background = () => {
+    return (
+        <div className="h-screen -z-1 ">
+            <Canvas >
+                <OrbitControls />   
+                <ambientLight intensity={.2}/>
+                <directionalLight position={[-2, 5, 2]} intensity={1}/>
+                <Speaker />
+            </Canvas>
+        </div>
+    )
+}
