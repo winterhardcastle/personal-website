c420c3b5bd7d0b2514463123a0dbae8ac2bb8a8d
Winter Hardcastle <winter@hardcastle.io>
2023-03-07 16:05:14 -0500

    Added resume button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,20 @@
+import { Button } from "@mui/material"
 import {Link} from "react-router-dom"
 
 const Navbar = () => {
     return (
         <div className="z-1 flex justify-between items-center">
             <a href='/home' className="p-2 text-3xl text-center font-bold">WH</a>
-            <div className="p-2 flex justify-between">
+            <div className="p-2 flex justify-center">
                 <Link to="/about">About</Link>
                 <Link className="px-12"to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link> 
+                <Link to="/contact">Contact</Link>
+            </div>
+            <div className="pl-12"> 
+                    <Button variant="outlined">Resume</Button>
             </div>
         </div>
-    )   
+    ) 
 }
 
 export default Navbar
