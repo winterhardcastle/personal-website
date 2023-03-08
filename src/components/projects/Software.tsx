d23a0a428da9c0c06db857d1a551260249f4c43c
Winter Hardcastle <winter@hardcastle.io>
2023-03-08 18:34:42 -0500

    Adding assets

--- src/components/projects/Software.tsx
+++ src/components/projects/Software.tsx
@@ -1,44 +1,97 @@
 const Software = () => {
     return (
         <div className="p-4 grid gap-4 grid-cols-3 grid-rows-3">  
-                    <div className="border border-dotted rounded-xl hover:bg-[url('')] flex-col">
-                        <h1 className="py-2 text-2xl text-bold uppercase">Fischer</h1>
-                        <p className="py-2 px-4">
-                            <span className="font-bold">Tech: </span> Typescript, NEXT.JS, Prisma, tailwindcss
-                        </p>
-                        <p className="py-2 px-4">
-                            Fischer is a fact-checking webapp and chrome extension that integrates NLP 
-                            and user contributions to determine the truthiness of information on the web.
-                        </p>
-                        <div className="flex justify-between align-center p-2">
-                            <a href="" className="no:hover pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
-                                <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
-                            </a>
-                            <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
-                                <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="border border-dotted rounded-xl hover:bg-[url('')] flex-col">
-                        <h1 className="py-2 text-2xl text-bold uppercase">Dive Data Depot</h1>
-                        <p className="py-2 px-4">
-                            <span className="font-bold">Tech: </span> Python, Pandas
-                        </p>
-                        <p className="py-2 px-4 text-bold">
-                            Developed for Anduril Industries(formerly Dive Technologies),  Dive Data Depot is a data aggregation 
-                            and analysis program that obtains critical mission statistics from AUV sensor data.
-
-                        </p>
-                        <div className="flex justify-between align-center p-2">
-                            <a href="" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
-                                <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
-                            </a>
-                            <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
-                                <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
-                            </a>
-                        </div>
-                    </div>
+            <div className="border border-dotted rounded-xl hover:bg-[url('')] flex-col">
+                <h1 className="pt-4 pb-2 text-2xl text-bold uppercase">Fischer</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Typescript, NEXT.JS, Prisma, tailwindcss
+                </p>
+                <p className="py-2 px-4">
+                    Fischer is a fact-checking webapp and chrome extension that integrates NLP 
+                    and user contributions to determine the truthiness of information on the web.
+                </p>
+                <div className="flex justify-between align-center p-2">
+                    <a href="https://github.com/orgs/FSA2211-Team-6/repositories" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
                 </div>
+            </div>
+            <div className="border border-dotted rounded-xl hover:bg-[url('')] flex-col">
+                <h1 className="pt-4 pb-2 text-2xl text-bold uppercase">Personal Website</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Typescript, React.js, Three.js, tailwindcss
+                </p>
+                <p className="py-2 px-4">
+                    This is the website that you are currently browsing, and functions as a portfolio of the most
+                    meaningful work that I have done! 
+                </p>
+                <div className="flex justify-between align-center p-2">
+                    <a href="https://github.com/winterhardcastle/personal-website" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div>
+            </div>
+            <div className="border border-dotted rounded-xl hover:bg-[url('')] flex-col">
+                <h1 className="pt-4 pb-2 text-2xl text-bold uppercase">Dive Data Depot</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Python, Pandas
+                </p>
+                <p className="py-2 px-4 text-bold">
+                    Developed for Anduril Industries(formerly Dive Technologies),  Dive Data Depot is a data aggregation 
+                    and analysis program that obtains critical mission statistics from AUV sensor data.
+                </p>
+                <div className="flex justify-between align-center p-2">
+                    <a href="" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div>
+            </div>
+            <div className="border border-dotted rounded-xl hover:bg-[url('')] flex-col">
+                <h1 className="pt-4 pb-2 text-2xl text-bold uppercase">Stock LED Display</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Python, Raspberry PI, 
+                </p>
+                <p className="py-2 px-4">
+                    This program displays a real-time visual representation of the success of your stock portfolio using Neopixel LED lights and
+                    the Robinhood API! 
+                </p>
+                <div className="flex justify-between align-center p-2">
+                    <a href="" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div>
+            </div>
+            <div className="border border-dotted rounded-xl hover:bg-[url('')] flex-col">
+                <h1 className="pt-4 pb-2 text-2xl text-bold uppercase">Coffee Castle</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Javascript, React, Sequelize, tailwindcss
+                </p>
+                <p className="py-2 px-4">
+                    An E-commerce website with a large collection of coffees for sale. My work on this project was mainly concentrated on the
+                    checkout feature, as well as the back-end/data models. 
+                </p>
+                <div className="flex justify-between align-center p-2">
+                    <a href="https://github.com/Team-Chrome/GraceShopper" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="https://graceshopper-uewg.onrender.com/products/6" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div>
+            </div>
+        </div>
     )
 }
 
