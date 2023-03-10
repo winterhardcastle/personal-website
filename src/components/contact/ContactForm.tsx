c27dab8e8e83b030793cbfc147a87cce344dbb0e
Winter Hardcastle <winter@hardcastle.io>
2023-03-10 14:22:46 -0500

    controlled form working

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,18 +1,22 @@
-import { useEffect, useState } from "react"
-import emailjs from '@emailjs/browser';
+import { useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
 
-interface Form {
-    firstName?: string;
-    lastName?: string;
-    email?: string;
-    phone?: string
-    msg?: string;
-}
+// interface Form {
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+//     phone: string
+//     msg: string;
+// }
 
 const ContactForm = () => {
-    const [form, setForm] = useState<Form>()
+    const [form, setForm] = useState({firstName:"",lastName:"", email:"",phone:"",msg:"",})
         
-    emailjs.init(process.env.EMAILJS_PUBLIC_KEY)
+    emailjs.init(import.meta.env.EMAILJS_PUBLIC_KEY || "")
+
+    const handleChange = (evt:any) => {
+        setForm({ ...form, [evt.target.name]: evt.target.value });
+    };
 
     const handleSubmit = (evt:any) => {
         evt.preventDefault();
@@ -24,8 +28,9 @@ const ContactForm = () => {
             email: form.email,
             phone: form.phone,
             msg: form.msg,
-            }, process.env.EMAILJS_PUBLIC_KEY);
+            }, import.meta.env.EMAILJS_PUBLIC_KEY);
     }
+
     useEffect(() => {
         console.log(form)
     })
@@ -35,34 +40,34 @@ const ContactForm = () => {
             <h1 className="text-5xl p-24">Contact Me!</h1>
             <form onSubmit={handleSubmit} className="w-1/2 grid gap-4 grid-rows-5 grid-cols-2"> 
                 <input 
-                name="firstname"
+                name="firstName"
                 type="text" placeholder="First Name" 
-                value={form?.firstName}
-                onChange={(e) => setForm({firstName: e.target.value})}
+                value={form.firstName}
+                onChange={handleChange}
                 />
                 <input 
-                name="lastname"
+                name="lastName"
                 type="text" placeholder="Last Name" 
-                value={form?.lastName}
-                onChange={(e) => setForm({lastName: e.target.value})}
+                value={form.lastName}
+                onChange={handleChange}
                 />
                 <input className="col-span-2"
                 name="email"
                 type="text" placeholder="Email" 
-                value={form?.email}
-                onChange={(e) => setForm({email: e.target.value})}
+                value={form.email}
+                onChange={handleChange}
                 />
                 <input className="col-span-2"
                 name="phone"
                 type="text" placeholder="Phone Number" 
-                value={form?.phone}
-                onChange={(e) => setForm({phone: e.target.value})}
+                value={form.phone}
+                onChange={handleChange}
                 />
                 <input className="col-span-2 row-span-2"
-                name="message"
+                name="msg"
                 type="text" placeholder="Message" 
-                value={form?.msg}
-                onChange={(e) => setForm({msg: e.target.value})}
+                value={form.msg}
+                onChange={handleChange}
                 />
             </form>
         </div>
