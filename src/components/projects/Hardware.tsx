e2625b639b4d770809fc560df2b0420fda202ad2
Winter Hardcastle <winter@hardcastle.io>
2023-03-17 19:45:46 -0400

    Added Hardware Projects section

--- src/components/projects/Hardware.tsx
+++ src/components/projects/Hardware.tsx
@@ -1,7 +1,120 @@
 const Hardware = () => {
     return (
-        <div>
-            hardware
+        <div className="p-4 grid gap-6 md:grid-cols-3 grid-cols-2 grid-rows-2"> 
+            <div className="transition ease-in-out group z-0 relative rounded-xl bg-cover hover:bg-[url('')] flex-col">
+                <div className="rounded-xl invisible group-hover:visible -z-10 absolute h-full w-full top-0 left-0 bg-[rgba(0,0,0,.6)]"></div>
+                    <div>  
+                        <h1 className="pb-2 pt-4 text-2xl font-bold uppercase">Wellness Concepts Media Prod.</h1>
+                        <p className="py-2 px-4">
+                            <span className="font-bold">Tech: </span> Fusion360, 3D printing, Laser cutting
+                        </p>
+                        <p className="max-md:hidden py-2 px-4">
+                            I designed and built a compact, portable, dual stream video and audio production interface that
+                            can be deployed and used as backup in minutes if the main systems fails. 
+                        </p>
+                    </div>   
+                {/* <div className="flex justify-between align-center p-2">
+                    <a href="https://github.com/orgs/FSA2211-Team-6/repositories" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="https://faction-fischer.vercel.app/posts" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div> */}
+            </div>
+            <div className="group z-0 relative rounded-xl bg-cover hover:bg-[url('')] flex-col">
+                <div className="rounded-xl invisible group-hover:visible -z-10 absolute h-full w-full top-0 left-0 bg-[rgba(0,0,0,.6)]"></div>
+                <h1 className="pb-2 pt-4 text-2xl text-bold uppercase">Hi-Fi Speakers</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Fusion360, LF-3D printing, Laser cutting, Audio Design
+                </p>
+                <p className="max-md:hidden py-2 px-4">
+                    I designed and built speakers with Bauhaus and MCM design elements. The internals are engineered to create
+                    the perfect sonic environment for the speaker driver that is utilized in the final build. 
+                </p>
+                {/* <div className="flex justify-between align-center p-2">
+                    <a href="https://github.com/winterhardcastle/personal-website" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div> */}
+            </div>
+            <div className="group z-0 relative rounded-xl bg-cover hover:bg-[url('')] flex-col">
+                <div className="rounded-xl invisible group-hover:visible -z-10 absolute h-full w-full top-0 left-0 bg-[rgba(0,0,0,.6)]"></div>
+                <h1 className="pb-2 pt-4 text-2xl text-bold uppercase">Pro Workforce Development</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Fusion360, 3D printing
+                </p>
+                <p className="max-md:hidden py-2 px-4 text-bold">
+                    Co-designed and engineered parts for a system that reduces repeated stress injuries in an industrial environment. Prototype was implemented
+                    on-site and deemed effective by a physical therapist specializing in workplace injury prevention.
+                </p>
+                {/* <div className="flex justify-between align-center p-2">
+                    <a href="" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div> */}
+            </div>
+            <div className="group z-0 relative rounded-xl bg-cover hover:bg-[url('')] flex-col">
+                <div className="rounded-xl invisible group-hover:visible -z-10 absolute h-full w-full top-0 left-0 bg-[rgba(0,0,0,.6)]"></div>
+                <h1 className="pb-2 pt-4 text-2xl text-bold uppercase">Custom Keyboard</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Fusion360, 3D printing, C++
+                </p>
+                <p className="max-md:hidden py-2 px-4">
+                    Designed and built a custom mechanical keyboard with a unique layout, and built custom firmware for it's microcontroller. 
+                </p>
+                {/* <div className="flex justify-between align-center p-2">
+                    <a href="" className="pt-2 pb-4 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="" className="pt-2 pb-4 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div> */}
+            </div>
+            <div className="group z-0 relative rounded-xl bg-cover hover:bg-[url('')] flex-col">
+                <div className="rounded-xl invisible group-hover:visible -z-10 absolute h-full w-full top-0 left-0 bg-[rgba(0,0,0,.6)]"></div>
+                <h1 className="pb-2 pt-4 text-2xl text-bold uppercase">Keyboard Wall Mounts</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Fusion360, 3D printing
+                </p>
+                <p className="max-md:hidden py-2 px-4">
+                    Designed univeral keyboard wall mounts. The 3D model has been downloaded by 53 users and has a 5-star rating on Cults3D.com
+                </p>
+                {/* <div className="flex justify-between align-center">
+                    <a href="https://github.com/Team-Chrome/GraceShopper" className="pt-2 pb-2 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="https://graceshopper-uewg.onrender.com/products/6" className="pt-2 pb-2 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div> */}
+            </div>
+            <div className="group z-0 relative rounded-xl bg-cover hover:bg-[url('')] flex-col">
+                <div className="rounded-xl invisible group-hover:visible -z-10 absolute h-full w-full top-0 left-0 bg-[rgba(0,0,0,.6)]"></div>
+                <h1 className="pb-2 pt-4 text-2xl text-bold uppercase">Shot-lever Bike Brakes</h1>
+                <p className="py-2 px-4">
+                    <span className="font-bold">Tech: </span> Fusion360, Metal and Carbon Fiber 3D printing
+                </p>
+                <p className="max-md:hidden py-2 px-4">
+                    Designed and built shot-lever style brakes that use two levers to actuate a single brake. In testing, 
+                    they withstood hundreds of miles of hard use.
+                </p>
+                {/* <div className="flex justify-between align-center">
+                    <a href="https://github.com/Team-Chrome/GraceShopper" className="pt-2 pb-2 pl-24 " target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-github-1.svg" alt="Github Logo"/>
+                    </a>
+                    <a href="https://graceshopper-uewg.onrender.com/products/6" className="pt-2 pb-2 pr-24" target="_blank" rel="noopener noreferrer">
+                        <img width="40" height="40" src = "./src\assets\logos\iconmonstr-globe-8.svg" alt="Web Logo"/>
+                    </a>
+                </div> */}
+            </div>
         </div>
     )
 }
